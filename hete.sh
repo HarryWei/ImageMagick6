d7be4a3053d7a8c85a3eb34c0e06ee1af359f792
Weiwei Jia <harryxiyou@gmail.com>
2018-06-11 16:16:28 +0800

    fix pre-test workload

--- hete.sh
+++ hete.sh
@@ -9,6 +9,7 @@ ip2="192.168.122.98"
 ctx=$2
 
 thread_num=$1
+export MAGICK_THREAD_LIMIT=$1
 
 #mount disk
 #ssh $kvm1@$ip1 "/home/$kvm1/vMigrater/scripts/mount.sh"
@@ -19,18 +20,27 @@ compare_bin="$HOME_DIR/compare"
 convert_bin="$HOME_DIR/convert"
 mogrify_bin="$HOME_DIR/mogrify"
 montage_bin="$HOME_DIR/montage"
-m122="$HOME_DIR/11m.jpg"
+m122="$HOME_DIR/5m.jpg"
 m78="$HOME_DIR/78m.jpg"
 k150="$HOME_DIR/150k.jpg"
 tmp="$HOME_DIR/tmp.jpg"
 
+#convert -limit memory 2mb -limit map 2mb -limit disk 2gb \
+#	  -define registry:cache:hosts=192.168.100.50:6668,192.168.100.51:6668 \
+#	    myhugeimage.jpg -sharpen 5x2 myhugeimage.png
+#
+
+para="-limit memory 2gb -limit map 2gb -limit disk 2gb"
+
 function remove_tmp() {
-    ssh $kvm1@$ip1 "rm $HOME_DIR/122m.jpg~"
-    ssh $kvm1@$ip1 "rm $HOME_DIR/122m.jpg~~"
-    ssh $kvm1@$ip1 "rm $HOME_DIR/122m.jpg~~~"
-    ssh $kvm2@$ip2 "rm $HOME_DIR/122m.jpg~"
-    ssh $kvm2@$ip2 "rm $HOME_DIR/122m.jpg~~"
-    ssh $kvm2@$ip2 "rm $HOME_DIR/122m.jpg~~~"
+    ssh $kvm1@$ip1 "cp $HOME_DIR/original.jpg $HOME_DIR/5m.jpg" &>> /dev/null
+    ssh $kvm1@$ip1 "rm $HOME_DIR/122m.jpg~" &>> /dev/null
+    ssh $kvm1@$ip1 "rm $HOME_DIR/122m.jpg~~" &>> /dev/null
+    ssh $kvm1@$ip1 "rm $HOME_DIR/122m.jpg~~~" &>> /dev/null
+    ssh $kvm2@$ip2 "cp $HOME_DIR/original.jpg $HOME_DIR/5m.jpg" &>> /dev/null
+    ssh $kvm2@$ip2 "rm $HOME_DIR/122m.jpg~" &>> /dev/null
+    ssh $kvm2@$ip2 "rm $HOME_DIR/122m.jpg~~" &>> /dev/null
+    ssh $kvm2@$ip2 "rm $HOME_DIR/122m.jpg~~~" &>> /dev/null
 }
 
 #compare
@@ -73,7 +83,7 @@ sleep 5
 ssh $kvm2@$ip2 "/home/$kvm2/parsec-3.0/parsec/vPair/inVM/accurate_cpu" >> $ctx/vm2.convert.edge_detect &
 for i in 1 2 3 4 5
 do
-	ssh $kvm1@$ip1 "time taskset -c 0-22 $convert_bin $m122 -colorspace Gray  -edge 1 -negate $tmp" &>> $ctx/vm1.convert.edge_detect
+	ssh $kvm1@$ip1 "time taskset -c 0-22 $convert_bin $para $m122 -colorspace Gray  -edge 1 -negate $tmp" &>> $ctx/vm1.convert.edge_detect
 	#ssh $kvm2@$ip2 "time taskset -c 0-22 $convert_bin $m122 -colorspace Gray  -edge 1 -negate $tmp" &>> $ctx/vm2.convert.edge_detect
 	#sleep 10
 done
--- homo.sh
+++ homo.sh
@@ -19,6 +19,7 @@ ctx=$2
 
 #TODO: will be used to represent thread nums.
 thread_num=$1
+export MAGICK_THREAD_LIMIT=$1
 
 #mount disk
 #ssh $kvm1@$ip1 "/home/$kvm1/vMigrater/scripts/mount.sh"
@@ -29,18 +30,20 @@ compare_bin="$HOME_DIR/compare"
 convert_bin="$HOME_DIR/convert"
 mogrify_bin="$HOME_DIR/mogrify"
 montage_bin="$HOME_DIR/montage"
-m122="$HOME_DIR/11m.jpg"
+m122="$HOME_DIR/5m.jpg"
 m78="$HOME_DIR/78m.jpg"
 k150="$HOME_DIR/150k.jpg"
 tmp="$HOME_DIR/tmp.jpg"
 
 function remove_tmp() {
-    ssh $kvm1@$ip1 "rm $HOME_DIR/122m.jpg~"
-    ssh $kvm1@$ip1 "rm $HOME_DIR/122m.jpg~~"
-    ssh $kvm1@$ip1 "rm $HOME_DIR/122m.jpg~~~"
-	ssh $kvm2@$ip2 "rm $HOME_DIR/122m.jpg~"
-	ssh $kvm2@$ip2 "rm $HOME_DIR/122m.jpg~~"
-	ssh $kvm2@$ip2 "rm $HOME_DIR/122m.jpg~~~"
+    ssh $kvm1@$ip1 "cp $HOME_DIR/original.jpg $HOME_DIR/5m.jpg" &>> /dev/null
+    ssh $kvm1@$ip1 "rm $HOME_DIR/122m.jpg~" &>> /dev/null
+    ssh $kvm1@$ip1 "rm $HOME_DIR/122m.jpg~~" &>> /dev/null
+    ssh $kvm1@$ip1 "rm $HOME_DIR/122m.jpg~~~" &>> /dev/null
+    ssh $kvm2@$ip2 "cp $HOME_DIR/original.jpg $HOME_DIR/5m.jpg" &>> /dev/null
+	ssh $kvm2@$ip2 "rm $HOME_DIR/122m.jpg~" &>> /dev/null
+	ssh $kvm2@$ip2 "rm $HOME_DIR/122m.jpg~~" &>> /dev/null
+	ssh $kvm2@$ip2 "rm $HOME_DIR/122m.jpg~~~" &>> /dev/null
 }
 
 ##compare
@@ -48,7 +51,7 @@ for i in 1 2 3 4 5
 do
 	ssh $kvm1@$ip1 "time taskset -c 0-22 $compare_bin $m122 $k150 $tmp" &>> $ctx/vm1.compare &
 	ssh $kvm2@$ip2 "time taskset -c 0-22 $compare_bin $m122 $k150 $tmp" &>> $ctx/vm2.compare
-	sleep 60
+	sleep 10
 done
 remove_tmp
 
@@ -57,7 +60,7 @@ for i in 1 2 3 4 5
 do
 	ssh $kvm1@$ip1 "time taskset -c 0-22 $compare_bin -channel red -metric PSNR $m122 $k150 $tmp" &>> $ctx/vm1.compare.channel_red &
 	ssh $kvm2@$ip2 "time taskset -c 0-22 $compare_bin -channel red -metric PSNR $m122 $k150 $tmp" &>> $ctx/vm2.compare.channel_red
-	sleep 30
+	sleep 10
 done
 remove_tmp
 
@@ -66,7 +69,7 @@ for i in 1 2 3 4 5
 do
 	ssh $kvm1@$ip1 "time taskset -c 0-22 $compare_bin -compose src $m122 $k150 $tmp" &>> $ctx/vm1.compare.compose &
 	ssh $kvm2@$ip2 "time taskset -c 0-22 $compare_bin -compose src $m122 $k150 $tmp" &>> $ctx/vm2.compare.compose
-	sleep 30
+	sleep 10
 done
 remove_tmp
 
@@ -75,7 +78,7 @@ for i in 1 2 3 4 5
 do
 	ssh $kvm1@$ip1 "time taskset -c 0-22 $convert_bin $m122 -colorspace Gray  -edge 1 -negate $tmp" &>> $ctx/vm1.convert.edge_detect &
 	ssh $kvm2@$ip2 "time taskset -c 0-22 $convert_bin $m122 -colorspace Gray  -edge 1 -negate $tmp" &>> $ctx/vm2.convert.edge_detect
-	sleep 30
+	sleep 10
 done
 remove_tmp
 
@@ -84,7 +87,7 @@ for i in 1 2 3 4 5
 do
 	ssh $kvm1@$ip1 "time taskset -c 0-22 $convert_bin $m122 -blur 0x8 $tmp" &>> $ctx/vm1.convert.blur &
 	ssh $kvm2@$ip2 "time taskset -c 0-22 $convert_bin $m122 -blur 0x8 $tmp" &>> $ctx/vm2.convert.blur
-	sleep 30
+	sleep 10
 done
 remove_tmp
 
@@ -93,7 +96,7 @@ for i in 1 2 3 4 5
 do
 	ssh $kvm1@$ip1 "time taskset -c 0-22 $convert_bin $m122 -charcoal 3 $tmp" &>> $ctx/vm1.convert.charcoal &
 	ssh $kvm2@$ip2 "time taskset -c 0-22 $convert_bin $m122 -charcoal 3 $tmp" &>> $ctx/vm2.convert.charcoal
-	sleep 30
+	sleep 10
 done
 remove_tmp
 
@@ -102,7 +105,7 @@ for i in 1 2 3 4 5
 do
 	ssh $kvm1@$ip1 "time taskset -c 0-22 $convert_bin -size 15360x8640 gradient:green-yellow $tmp" &>> $ctx/vm1.convert.draw &
 	ssh $kvm2@$ip2 "time taskset -c 0-22 $convert_bin -size 15360x8640 gradient:green-yellow $tmp" &>> $ctx/vm2.convert.draw
-	sleep 30
+	sleep 10
 done
 remove_tmp
 
@@ -111,7 +114,7 @@ for i in 1 2 3 4 5
 do
 	ssh $kvm1@$ip1 "time taskset -c 0-22 $convert_bin $m122 -morphology CloseI Disk:2.5 $tmp" &>> $ctx/vm1.convert.paint_effect &
 	ssh $kvm2@$ip2 "time taskset -c 0-22 $convert_bin $m122 -morphology CloseI Disk:2.5 $tmp" &>> $ctx/vm2.convert.paint_effect
-	sleep 30
+	sleep 10
 done
 remove_tmp
 
@@ -120,7 +123,7 @@ for i in 1 2 3 4 5
 do
 	ssh $kvm1@$ip1 "time taskset -c 0-22 $convert_bin $m122 -sharpen 0x1 $tmp" &>> $ctx/vm1.convert.sharpen &
 	ssh $kvm2@$ip2 "time taskset -c 0-22 $convert_bin $m122 -sharpen 0x1 $tmp" &>> $ctx/vm2.convert.sharpen
-	sleep 30
+	sleep 10
 done
 remove_tmp
 
@@ -129,7 +132,7 @@ for i in 1 2 3 4 5
 do
 	ssh $kvm1@$ip1 "time taskset -c 0-22 $convert_bin $m122 -fft +depth +adjoin $tmp" &>> $ctx/vm1.convert.fft &
 	ssh $kvm2@$ip2 "time taskset -c 0-22 $convert_bin $m122 -fft +depth +adjoin $tmp" &>> $ctx/vm2.convert.fft
-	sleep 30
+	sleep 10
 done
 remove_tmp
 
@@ -138,16 +141,16 @@ for i in 1 2 3 4 5
 do
 	ssh $kvm1@$ip1 "time taskset -c 0-22 $convert_bin $m122 -background Blue  -shear 20x0 $tmp" &>> $ctx/vm1.convert.shear &
 	ssh $kvm2@$ip2 "time taskset -c 0-22 $convert_bin $m122 -background Blue  -shear 20x0 $tmp" &>> $ctx/vm2.convert.shear
-	sleep 30
+	sleep 10
 done
 remove_tmp
 
 ##mogrify resize
 for i in 1 2 3 4 5
 do
-	ssh $kvm1@$ip1 "time taskset -c 0-22 $mogrify_bin -resize 150% $m122" &>> $ctx/vm1.mogrify.resize &
-	ssh $kvm2@$ip2 "time taskset -c 0-22 $mogrify_bin -resize 150% $m122" &>> $ctx/vm2.mogrify.resize
-	sleep 30
+	ssh $kvm1@$ip1 "time taskset -c 0-22 $mogrify_bin -resize 50% $m122" &>> $ctx/vm1.mogrify.resize &
+	ssh $kvm2@$ip2 "time taskset -c 0-22 $mogrify_bin -resize 50% $m122" &>> $ctx/vm2.mogrify.resize
+	sleep 10
 done
 remove_tmp
 
@@ -156,7 +159,7 @@ for i in 1 2 3 4 5
 do
 	ssh $kvm1@$ip1 "time taskset -c 0-22 $mogrify_bin -segment 128000x60 $m78" &>> $ctx/vm1.mogrify.segment &
 	ssh $kvm2@$ip2 "time taskset -c 0-22 $mogrify_bin -segment 128000x60 $m78" &>> $ctx/vm2.mogrify.segment
-	sleep 30
+	sleep 10
 done
 remove_tmp
 
@@ -165,6 +168,6 @@ for i in 1 2 3 4 5
 do
 	ssh $kvm1@$ip1 "time taskset -c 0-22 $montage_bin $m122 $k150 $tmp" &>> $ctx/vm1.montage &
 	ssh $kvm2@$ip2 "time taskset -c 0-22 $montage_bin $m122 $k150 $tmp" &>> $ctx/vm2.montage
-	sleep 30
+	sleep 10
 done
 remove_tmp
